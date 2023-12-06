2bfdaa7b3a2b7b97280ae1c5177e90a9fc0578e5
matias-rojas-nest <matias.rojas@nestconsultores.cl>
2023-12-06 16:24:34 -0300

    Add folder structure

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Matías Branch</Text>
+      <Text>Matías Bra</Text>
       <StatusBar style="auto" />
     </View>
   );
